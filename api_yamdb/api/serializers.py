a3ac95c17a1e6fc2aae7f3a1829f3773b69791a1
Дмитрий Венде <vende.d@mail.ru>
2023-03-12 13:39:54 +0300

    z

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
-
 from reviews.models import Category, Genre, Title, Review, Comment
 from users.models import User
 
